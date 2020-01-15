f1617b71985c3a79cb4042ba962fb6e4b8a41d3a
Arthurlemus <arthurlemus@hotmail.com>
2020-01-14 22:21:28 -0600

    nada que ver

--- 01-myApp/src/app/tab1/tab1.page.ts
+++ 01-myApp/src/app/tab1/tab1.page.ts
@@ -1,22 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page implements OnInit {
+export class Tab1Page {
 
-  constructor() { }
-
-  ngOnInit() {
-    debugger;
-     let numero = 10;
-     numero += 10;
-     numero *= 2;
-     numero -= 10;
-     console.log({numero});
-
-  }
+  constructor() {}
 
 }
