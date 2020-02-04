5ddcc5c7e257f468c7c1ef7084fe0f5d081e9523
Arthurlemus <arthurlemus@hotmail.com>
2020-02-04 01:25:15 -0600

    Segment
    Se termino la parte del componente Segment

--- 03-componentes/src/app/pages/progress-bar/progress-bar.page.ts
+++ 03-componentes/src/app/pages/progress-bar/progress-bar.page.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./progress-bar.page.scss'],
 })
 export class ProgressBarPage implements OnInit {
-
+  porcentaje: number;
   constructor() { }
 
   ngOnInit() {
   }
-
+  cambioRango(event) {
+    this.porcentaje = (event.detail.value / 100);
+  }
 }
--- 03-componentes/src/app/services/data.service.ts
+++ 03-componentes/src/app/services/data.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Componente } from '../interfaces/interfaces';
+import { delay } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +19,12 @@ export class DataService {
   getMenu() {
     return this.http.get<Componente[]>('/assets/data/menu.json');
   }
+  
+  getAlbums() {
+    return this.http.get<any[]>('https://jsonplaceholder.typicode.com/albums');
+  }
 
+  getHeroes() {
+    return this.http.get('/assets/data/superheroes.json').pipe(delay(5000));
+  }
 }
